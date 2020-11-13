6dff33de4a7e585f8485e0533d69507f6c1ba4ab
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-13 19:30:45 +0300

    Added comments

--- src/CredcalcService.java
+++ src/CredcalcService.java
@@ -3,7 +3,7 @@ public class CredcalcService {
     public float calculate(int s, int n, float m) {
         float i = m / 100 / 12; //ежемесячная процентная ставка
         float p = (1 + i);
-        float r = (float) Math.pow(p, n);
+        float r = (float) Math.pow(p, n); //возведение в степень
         float k = i + (i / (r - 1));
         float pay = s * k;
         return pay;
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         CredcalcService service = new CredcalcService();
         int s = 1_955_000; //сумма кредита
-        int n = 360; //срок, на который берется кредит, месцев
-        float m = 6.5f; // годовая процентная ставка
+        int n = 240; //срок, на который берется кредит, месцев
+        float m = 8.0f; // годовая процентная ставка
         float pay = service.calculate(s, n, m);
         System.out.print("Ежемесячный платеж:  ");
-        System.out.printf("%.2f", pay);
+        System.out.printf("%.2f", pay); //ограничение двух знаков после запятой
     }
 }
