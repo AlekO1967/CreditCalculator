29997426a58ad42ddc299103613aa9c2e424ee63
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-13 16:24:38 +0300

    Created new method

---
+++ src/CredcalcService.java
@@ -0,0 +1,11 @@
+public class CredcalcService {
+
+    public float calculate(int s, int n, float m) {
+        float i = m / 100 / 12; //ежемесячная процентная ставка
+        float p = (1 + i);
+        float r = (float) Math.pow(p, n);
+        float k = i + (i / (r - 1));
+        float pay = s * k;
+        return pay;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        CredcalcService service = new CredcalcService();
         int s = 1_955_000; //сумма кредита
-        int n = 240; //срок, на который берется кредит, месцев
-        float m = 8.0f; // годовая процентная ставка
-        float i = m / 100 / 12; //ежемесячная процентная ставка
-        float p = (1 + i);
-        float r = (float) Math.pow(p, n);
-        float k = i + (i / (r - 1));
-        float pay = s * k;
+        int n = 360; //срок, на который берется кредит, месцев
+        float m = 6.5f; // годовая процентная ставка
+        float pay = service.calculate(s, n, m);
         System.out.print("Ежемесячный платеж:  ");
         System.out.printf("%.2f", pay);
     }
